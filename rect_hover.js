7f1baa414564ded866e945d666a7e1508076dd5d
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-02 14:06:01 +0530

    Added hover on rect shape

---
+++ rect_hover.js
@@ -0,0 +1,29 @@
+let _rect = {
+    x: 100,
+    y: 100,
+    width: 150,
+    height: 150
+}
+
+function setup() {
+    createCanvas(600, 400);
+}
+
+function draw() {
+    background(0);
+    strokeWeight(3);
+    stroke(255);
+    fill(0)
+    
+    if (
+        mouseX > _rect.x - _rect.width / 2 &&
+        mouseX < _rect.x + _rect.width / 2 &&
+        mouseY > _rect.y - _rect.height / 2 &&
+        mouseY < _rect.y + _rect.height / 2
+    ) {
+        fill(100, 100, 200);
+    }
+
+    rectMode(CENTER)
+    rect(_rect.x, _rect.y, _rect.width, _rect.height);
+}
