5e1387ff7162d5cde6865a09a83d71bf7d825f70
AmanSilawat <aman.khan.silawat.aks@gmail.com>
2020-12-03 12:19:34 +0530

    Added shaking balls

---
+++ shaking_balls.js
@@ -0,0 +1,40 @@
+let _canvas = {
+    width: 600,
+    height: 400,
+    shaking_ball: [],
+    balls: 50
+}
+
+function setup() {
+    createCanvas(_canvas.width, _canvas.height);
+    for (let i = 0; i < _canvas.balls; i++) {
+        _canvas.shaking_ball.push(new Bubble());
+    }
+}
+
+function draw() {
+    background(0);
+    for (const ball of _canvas.shaking_ball) {
+        ball.move();
+        ball.show();
+    }
+}
+
+class Bubble {
+    constructor() {
+        this.x = random(0, width);
+        this.y = random(0, height);
+    }
+
+    move() {
+        this.x = this.x + random(-5, 5);
+        this.y = this.y + random(-5, 5);
+    }
+
+    show() {
+        stroke(255);
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
+    }
+}
